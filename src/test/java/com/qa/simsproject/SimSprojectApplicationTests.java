87ebff506a4fc16424862a19e156d987da1ab8e0
srodrichu <sebastianawrchu@gmail.com>
2022-03-22 16:25:19 +0000

    SC: Spring boot + folder structure main/java

---
+++ src/test/java/com/qa/simsproject/SimSprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.simsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimSprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
