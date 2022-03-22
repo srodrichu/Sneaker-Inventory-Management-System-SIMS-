3f851e03daf9561e1ea9e10c51ee5996f5ec55cd
srodrichu <sebastianawrchu@gmail.com>
2022-03-22 19:57:48 +0000

    SC: added controller class + constructor

---
+++ src/main/java/com/qa/simsproject/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.qa.simsproject.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.simsproject.services.Services;
+
+@RestController
+public class Controller {
+	
+	private Services service;
+	
+	
+	
+}
