e819f42e2ea9233cd0821bca6c4a3593b94f2038
srodrichu <sebastianawrchu@gmail.com>
2022-03-22 16:40:54 +0000

    SC: model getters/setters, toString, hashcode and equals

--- src/main/java/com/qa/simsproject/model/SneakerEntry.java
+++ src/main/java/com/qa/simsproject/model/SneakerEntry.java
@@ -1,5 +1,7 @@
 package com.qa.simsproject.model;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -44,6 +46,91 @@ public class SneakerEntry {
 		this.listedPrice = listedPrice;
 		this.isSold = isSold;
 	}
+	
+
+	public SneakerEntry(String name, float size, float costPrice, float listedPrice, boolean isSold) {
+		super();
+		this.name = name;
+		this.size = size;
+		this.costPrice = costPrice;
+		this.listedPrice = listedPrice;
+		this.isSold = isSold;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getSize() {
+		return size;
+	}
+
+	public void setSize(float size) {
+		this.size = size;
+	}
+
+	public float getCostPrice() {
+		return costPrice;
+	}
+
+	public void setCostPrice(float costPrice) {
+		this.costPrice = costPrice;
+	}
+
+	public float getListedPrice() {
+		return listedPrice;
+	}
+
+	public void setListedPrice(float listedPrice) {
+		this.listedPrice = listedPrice;
+	}
+
+	public boolean isSold() {
+		return isSold;
+	}
+
+	public void setSold(boolean isSold) {
+		this.isSold = isSold;
+	}
+
+	@Override
+	public String toString() {
+		return "SneakerEntry [id=" + id + ", name=" + name + ", size=" + size + ", costPrice=" + costPrice
+				+ ", listedPrice=" + listedPrice + ", isSold=" + isSold + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(costPrice, id, isSold, listedPrice, name, size);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SneakerEntry other = (SneakerEntry) obj;
+		return Float.floatToIntBits(costPrice) == Float.floatToIntBits(other.costPrice) && id == other.id
+				&& isSold == other.isSold
+				&& Float.floatToIntBits(listedPrice) == Float.floatToIntBits(other.listedPrice)
+				&& Objects.equals(name, other.name) && Float.floatToIntBits(size) == Float.floatToIntBits(other.size);
+	}
+	
 	
 	
 
