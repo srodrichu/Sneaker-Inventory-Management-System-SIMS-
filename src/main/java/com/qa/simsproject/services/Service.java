d6a7de3fe9fa9cbae9d7b3815ba431a8a2f8c36e
srodrichu <sebastianawrchu@gmail.com>
2022-03-22 17:12:54 +0000

    SC: service imports + constructor

--- src/main/java/com/qa/simsproject/services/Service.java
+++
@@ -1,5 +0,0 @@
-package com.qa.simsproject.services;
-
-public class Service {
-
-}
---
+++ src/main/java/com/qa/simsproject/services/Services.java
@@ -0,0 +1,19 @@
+package com.qa.simsproject.services;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.simsproject.repo.Repo;
+
+@Service
+public class Services {
+	
+	private Repo repo;
+
+	public Services() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
