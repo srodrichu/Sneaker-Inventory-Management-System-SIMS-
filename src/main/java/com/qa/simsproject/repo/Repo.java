9b3f5b3f6b86a1c5d42138aef5dd42229fdf73ec
srodrichu <sebastianawrchu@gmail.com>
2022-03-22 18:13:17 +0000

    SC: added custom queries to repo

--- src/main/java/com/qa/simsproject/repo/Repo.java
+++ src/main/java/com/qa/simsproject/repo/Repo.java
@@ -1,9 +1,18 @@
 package com.qa.simsproject.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.qa.simsproject.model.SneakerEntry;
 
 public interface Repo extends JpaRepository<SneakerEntry, Long>{
 
+	public List<SneakerEntry> findBySizeBetweenAndIsSoldFalse(float startSize, float endSize);
+	
+	public List<SneakerEntry> findByPriceLessThanEqualAndIsSoldFalse(float price);
+	
+	public List<SneakerEntry> findByIsSoldFalse();
+	
+	
 }
