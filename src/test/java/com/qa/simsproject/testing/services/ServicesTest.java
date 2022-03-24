971a6009ec344a6629719862d60ba74c14e0f3e9
srodrichu <sebastianawrchu@gmail.com>
2022-03-24 13:48:32 +0000

    SC: Service tests complete

--- src/test/java/com/qa/simsproject/testing/services/ServicesTest.java
+++ src/test/java/com/qa/simsproject/testing/services/ServicesTest.java
@@ -41,9 +41,9 @@ public class ServicesTest {
 		
 		Mockito.when(repo.save(sneaker1)).thenReturn(sneaker1Id);
 		
-		boolean result = services.createEntry(sneaker1);
+		SneakerEntry result = services.createEntry(sneaker1);
 		
-		Assertions.assertTrue(result);
+		Assertions.assertEquals(sneaker1Id, result);;
 		
 	}
 	
@@ -71,23 +71,95 @@ public class ServicesTest {
 		
 	}
 	
-//	@Test
-//	public void testUpdateById() {
-//		
-//		//GIVEN	
+	@Test
+	public void testUpdateById() {
+		
+		long id = 2l;
+		
+		SneakerEntry updatedIn = new SneakerEntry("Jordan 3", 6.5f, 150.00f, 290.00f, false);
+		
+		SneakerEntry updatedOut = new SneakerEntry(id, updatedIn.getName(), updatedIn.getSize(), updatedIn.getCostPrice(), updatedIn.getListedPrice(), updatedIn.isSold());
+		
+		Optional<SneakerEntry> toUpdateOpt = Optional.of(sneaker2Id);
+		
+		Mockito.when(repo.findById(id)).thenReturn(toUpdateOpt);
+		Mockito.when(repo.save(updatedOut)).thenReturn(updatedOut);
+		
+		SneakerEntry result = services.updateById(id, updatedIn);
+		
+		Assertions.assertEquals(updatedOut, result);
+		
+	}
 	
-//		Mockito.when(repo.save(sneaker1)).thenReturn(sneaker1Id);
+	@Test
+	public void testDeleteById() {
+		
+		long id = 1L;
+		
+		Optional<SneakerEntry> deleteOpt = Optional.of(sneaker1Id);
+		
+		Mockito.when(repo.findById(id)).thenReturn(deleteOpt);
+		
+		SneakerEntry result = services.deleteById(id);
+		
+		Assertions.assertEquals(sneaker1Id, result);
+		
+//		Verify
+		
+		Mockito.verify(repo, Mockito.times(1)).findById(id);
+		Mockito.verify(repo, Mockito.times(1)).deleteById(id);
+		
+		
+	}
 	
-		//WHEN
-//		
-//		Mockito.when(repo.findById(2l).get()).thenReturn(sneaker2Id);
+	@Test
+	public void testMarkAsSold() {
+		
+		long id = 1L;
+		
+		Optional<SneakerEntry> opt = Optional.of(sneaker1Id);
+		
+		SneakerEntry soldOut = new SneakerEntry(id, sneaker1Id.getName(), sneaker1Id.getSize(), sneaker1Id.getCostPrice(), sneaker1Id.getListedPrice(), !sneaker1Id.isSold());
+		
+		
+		Mockito.when(repo.findById(id)).thenReturn(opt);
+		
+		Mockito.when(repo.save(soldOut)).thenReturn(soldOut);
+		
+		SneakerEntry result = services.markAsSoldById(id);
+		
+		Assertions.assertEquals(soldOut, result);
+		
+	}
 	
-		//THEN
-//		
-//		boolean result = services.updateById(2l, sneaker1);
-//		
-//	}
-//	
-//	@Test
-//	public void 
+	@Test
+	public void realProfitByid() {
+		
+		long id = 1L;
+		
+		float expected = sneaker1Id.getListedPrice() - sneaker1Id.getCostPrice();
+		
+		Optional<SneakerEntry> opt = Optional.of(sneaker1Id);
+		
+		Mockito.when(repo.findById(id)).thenReturn(opt);
+		
+		float result = services.profitById(id);
+	
+		Assertions.assertEquals(expected, result);
+			
+	}
+	
+	@Test
+	public void testDeleteAll() {
+		
+		boolean result = services.deleteAll();
+		
+		Assertions.assertTrue(result);
+		
+		Mockito.verify(repo, Mockito.times(1)).deleteAll();
+		
+		
+	}
+	
+	
 }
